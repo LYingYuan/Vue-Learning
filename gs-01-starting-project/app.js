37fd3f7386808dad5879e9ba25a67da7bd730372
LYingYuan <1970027041@qq.com>
2022-03-13 19:58:00 +0800

    first commit

---
+++ gs-01-starting-project/app.js
@@ -0,0 +1,13 @@
+// const buttonEL = document.querySelector("button");
+// const inputEL = document.querySelector("input");
+// const listEL = document.querySelector("ul");
+
+// function addGoal() {
+//     const entered_value = inputEL.value;
+//     const list_item = document.createElement("li");
+//     list_item.textContent = entered_value;
+//     listEL.appendChild(list_item);
+//     inputEL.value = "";
+// }
+
+// buttonEL.addEventListener("click", addGoal);

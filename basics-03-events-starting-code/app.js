12a1228753e09c5be2c88c1a9e5a0d70b0cba05b
LYingYuan <1970027041@qq.com>
2022-03-14 12:10:16 +0800

    保存学习进度

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,9 +1,29 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-    };
-  },
-});
+	data() {
+		return {
+			counter: 0,
+			name: '',
+			confirm_name: '',
+		}
+	},
+	methods: {
+		confirmName() {
+			this.confirm_name = this.name
+		},
+		submitForm(event) {
+			// event.preventDefault();
+			alert('submitted!')
+		},
+		setName(event, last_name) {
+			this.name = event.target.value + ' ' + last_name
+		},
+		add(num) {
+			this.counter = this.counter + num
+		},
+		reduce(num) {
+			this.counter = this.counter - num
+		},
+	},
+})
 
-app.mount('#events');
+app.mount('#events')
